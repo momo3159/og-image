ed5efa8597f0f869a2d5eef64d9d9f02edb961f6
tkawakita <tommycsth61@gmail.com>
2021-03-04 07:33:51 +0900

    ??

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -118,7 +118,7 @@ function getCss(theme: string, fontSize: string) {
 export function getHtml(parsedReq: ParsedRequest) {
   const { text, theme, md, fontSize } = parsedReq;
 
-  return `<!DOCTYPE html>
+  return (`<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
@@ -133,7 +133,7 @@ export function getHtml(parsedReq: ParsedRequest) {
             <svg id="bg-svg" width="100vw" viewBox="0 0 1440 600" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><defs><linearGradient id="gradient"><stop offset="5%" stop-color="#9900ef88"></stop><stop offset="95%" stop-color="#0693e388"></stop></linearGradient></defs><path d="M 0,600 C 0,600 0,200 0,200 C 80.71770334928229,204.59330143540672 161.43540669856458,209.1866028708134 258,199 C 354.5645933014354,188.8133971291866 466.97607655502395,163.84688995215308 572,171 C 677.023923444976,178.15311004784692 774.6602870813398,217.4258373205742 875,228 C 975.3397129186602,238.5741626794258 1078.3827751196172,220.44976076555022 1173,211 C 1267.6172248803828,201.55023923444978 1353.8086124401914,200.7751196172249 1440,200 C 1440,200 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="url(#gradient)" class="transition-all duration-300 ease-in-out delay-150"></path><defs><linearGradient id="gradient"><stop offset="5%" stop-color="#9900efff"></stop><stop offset="95%" stop-color="#0693e3ff"></stop></linearGradient></defs><path d="M 0,600 C 0,600 0,400 0,400 C 103.72248803827753,368.81339712918657 207.44497607655506,337.6267942583732 291,356 C 374.55502392344494,374.3732057416268 437.9425837320574,442.30622009569373 538,471 C 638.0574162679426,499.69377990430627 774.7846889952152,489.14832535885165 887,469 C 999.2153110047848,448.85167464114835 1086.9186602870814,419.10047846889955 1175,406 C 1263.0813397129186,392.89952153110045 1351.5406698564593,396.4497607655502 1440,400 C 1440,400 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="url(#gradient)" class="transition-all duration-300 ease-in-out delay-150"></path></svg>
         </div>
     </body>
-</html>`;
+</html>`);
 }
 
 // function getImage(src: string, width = "auto", height = "225") {

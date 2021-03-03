47a3b59d7cb433b62ad685e9735b8052d6ba9d71
tkawakita <tommycsth61@gmail.com>
2021-03-04 07:29:56 +0900

    typeの設定

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { parseRequest } from "./_lib/parser";
 import { getScreenshot } from "./_lib/chromium";
 import { getHtml } from "./_lib/template";
 
-const isDev = !process.env.AWS_REGION;
+const isDev = !process.env.AWS_REGION || false;
 const isHtmlDebug = process.env.OG_HTML_DEBUG === "1";
 
 export default async function handler(
